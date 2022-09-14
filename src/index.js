d22295dc298508fd34d1301a3cdf9496f80f25b4
henriquesdias <henrique.dias1915@gmail.com>
2022-09-14 15:22:21 -0300

    the inicial setup is ready, using database and express

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("tudo certo");
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
