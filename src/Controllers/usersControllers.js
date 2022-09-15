e15eea3bc938d66214b71dbbea8ab7d9ae8ff0b7
henriquesdias <henrique.dias1915@gmail.com>
2022-09-15 00:53:09 -0300

    the route 'sign-in' is configured. A token and username is returned

--- src/Controllers/usersControllers.js
+++ src/Controllers/usersControllers.js
@@ -1,10 +1,15 @@
 import db from "../Database/db.js";
 import joi from "joi";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 
 const schemaSignUp = joi.object({
   email: joi.string().email().required(),
   name: joi.string().required().trim().min(3),
+  password: joi.string().required().trim().min(4),
+});
+const schemaSignIn = joi.object({
+  email: joi.string().email().required(),
   password: joi.string().required().trim().min(4),
 });
 
@@ -31,4 +36,25 @@ async function signUp(req, res) {
     res.status(500).send(error.message);
   }
 }
-export { signUp };
+async function signIn(req, res) {
+  const { email, password } = req.body;
+  const validation = schemaSignIn.validate(
+    { email, password },
+    { abortEarly: false }
+  );
+  if (validation.error) {
+    return res.status(422).send(validation.error.details.map((e) => e.message));
+  }
+  try {
+    const user = await db.collection("users").findOne({ email });
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = uuid();
+      await db.collection("sessions").insertOne({ userID: user._id, token });
+      return res.status(200).send({ token, name: user.name });
+    }
+    res.status(404).send("Usuário não encontrado/senha inválida");
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+export { signUp, signIn };
